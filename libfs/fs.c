c2091735e88f1c8c6c5a79b2c97b4f6e14a5f9ac
Ashwin Muralidharan <ashwin.m.dharan@gmail.com>
2020-02-26 19:37:20 -0800

    Changed info to 4-06/2 to accurately interate through array

--- libfs/fs.c
+++ libfs/fs.c
@@ -10,7 +10,7 @@
 #define UNUSED(x) (void)(x)
 
 typedef struct __attribute__((__packed__)) {
-	int8_t signature[2];
+	int8_t signature[8];
 	int16_t totalBlocks;
 	int16_t rootDirBlockIndex;
 	int16_t dataBlockStartIndex;
@@ -46,11 +46,12 @@ int fs_mount(const char *diskname)
 	block_disk_open(diskname);
 	FS = malloc(sizeof(ECS150FS));
 	block_read(0, &FS->superblock);
-	char* sig = (char*)FS->superblock.signature;
-	if(strcmp("ECS150FS\0", sig) != 0){
+	char desiredSig[] = "ECS150FS";
+	const void* sig = FS->superblock.signature;
+	if(memcmp(desiredSig, sig, 2) != 0){
 		return -1;
 	}
-	FS->FAT = (int16_t*)malloc(FS->superblock.numFATBlocks*4096);
+	FS->FAT = malloc(FS->superblock.numFATBlocks*4096);
 	for(int i = 1; i<=FS->superblock.numFATBlocks; i++){
 		block_read(i, &FS->FAT[(i-1)*4096]);
 	}
@@ -74,20 +75,21 @@ int fs_info(void)
 	printf("fat_blk_count=%d\n", FS->superblock.numFATBlocks);
 	printf("rdir_blk=%d\n", FS->superblock.rootDirBlockIndex);
 	printf("data_blk=%d\n", FS->superblock.dataBlockStartIndex);
-	printf("data_blk_count%d\n", FS->superblock.numDataBlocks);
-	int i;
-	for(i = 0; i<FS->superblock.numFATBlocks*4096; i++){
-		if(FS->FAT[i] > 0){
-			i++;
+	printf("data_blk_count=%d\n", FS->superblock.numDataBlocks);
+	int fat_free = 0;
+	for(int i = 0; i<FS->superblock.numFATBlocks*(4096/2); i++){
+		if(FS->FAT[i] == 0){
+			fat_free++;
 		}
 	}
-	printf("fat_free_ratio=%d/%d\n", i, FS->superblock.numFATBlocks*4096);
-	for(i = 0; i<128; i++){
-		if((char)FS->rootDir.rootDirEntries[i].filename[0] != '\0'){
-			i++;
+	printf("fat_free_ratio=%d/%d\n", fat_free, FS->superblock.numFATBlocks*(4096/2));
+	int rdir_free = 0;
+	for(int i = 0; i<128; i++){
+		if((char)FS->rootDir.rootDirEntries[i].filename[0] == '\0'){
+			rdir_free++;
 		}
 	}
-	printf("rdir_free_ratio=%d/%d\n", i, 128);
+	printf("rdir_free_ratio=%d/%d\n", rdir_free, 128);
 	return 0;
 }
 
