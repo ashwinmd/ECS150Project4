f621cda3b018ccca373e3f6a993f9d0f1d5f8675
Ashwin Muralidharan <ashwin.m.dharan@gmail.com>
2020-02-26 18:06:52 -0800

    Rough Phase 1, trying to silence warnings

--- libfs/fs.c
+++ libfs/fs.c
@@ -45,14 +45,15 @@ int fs_mount(const char *diskname)
 	FS = malloc(sizeof(ECS150FS));
 	block_read(0, &FS->superblock);
 	char* sig = (char*)FS->superblock.signature;
-	if(sig != "ECS150FS"){
+	if(strcmp("ECS150FS\0", sig) != 0){
 		return -1;
 	}
-	FS.FAT = (int16_t*)malloc(FS->superblock.numFATBlocks*4096);
+	FS->FAT = (int16_t*)malloc(FS->superblock.numFATBlocks*4096);
 	for(int i = 1; i<=FS->superblock.numFATBlocks; i++){
 		block_read(i, &FS->FAT[(i-1)*4096]);
 	}
 	block_read(FS->superblock.rootDirBlockIndex, &FS->rootDir);
+	return 0;
 }
 
 int fs_umount(void)
@@ -67,68 +68,96 @@ int fs_umount(void)
 
 int fs_info(void)
 {
-	printf("total_blk_count=%d\n", FS->totalBlocks);
-	printf("fat_blk_count=%d\n", FS->numFATBlocks);
-	printf("rdir_blk=%d\n", FS->rootDirBlockIndex);
-	printf("data_blk=%d\n", FS->dataBlockStartIndex);
-	printf("data_blk_count%d\n", FS->numDataBlocks);
+	printf("total_blk_count=%d\n", FS->superblock.totalBlocks);
+	printf("fat_blk_count=%d\n", FS->superblock.numFATBlocks);
+	printf("rdir_blk=%d\n", FS->superblock.rootDirBlockIndex);
+	printf("data_blk=%d\n", FS->superblock.dataBlockStartIndex);
+	printf("data_blk_count%d\n", FS->superblock.numDataBlocks);
 	int i;
 	for(i = 0; i<FS->superblock.numFATBlocks*4096; i++){
 		if(FS->FAT[i] > 0){
 			i++;
 		}
 	}
-	printf("fat_free_ratio=%d/%d\n", i, FS->superblock->numFATBlocks*4096);
+	printf("fat_free_ratio=%d/%d\n", i, FS->superblock.numFATBlocks*4096);
 	for(i = 0; i<128; i++){
-		if(FS->(char)rootDir.rootDirEntries[i].filename[0] != '\0'){
+		if((char)FS->rootDir.rootDirEntries[i].filename[0] != '\0'){
 			i++;
 		}
 	}
-	printf("rdir_free_ratio=%d/%d\n", i, 128)
+	printf("rdir_free_ratio=%d/%d\n", i, 128);
+	return 0;
 }
 
 int fs_create(const char *filename)
 {
-	/* TODO: Phase 2 */
+	filename = NULL;
+	const char* newptr = filename;
+	return 0;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+	filename = NULL;
+	const char* newptr = filename;
+	return 0;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
+	filename = NULL;
+	const char* newptr = filename;
+	return 0;
 }
 
 int fs_open(const char *filename)
 {
 	/* TODO: Phase 3 */
+
+	filename = NULL;
+	const char* newptr = filename;
+	return 0;
 }
 
 int fs_close(int fd)
 {
 	/* TODO: Phase 3 */
+	fd++;
+	return 0;
 }
 
 int fs_stat(int fd)
 {
 	/* TODO: Phase 3 */
+	fd++;
+	return 0;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
 	/* TODO: Phase 3 */
+	fd++;
+	offset++;
+	return 0;
 }
 
 int fs_write(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+	fd++;
+	buf = NULL;
+	count++;
+	return 0;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+	fd++;
+	buf = NULL;
+	count++;
+	return 0;
 }
 
